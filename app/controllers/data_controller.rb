69e933ed969b47e07043e2fb52a9c06908f522dd
Isaac Peifer <iwpeifer@gmail.com>
2017-07-30 12:17:38 -0400

    added limit to params

--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -3,7 +3,7 @@ require 'rest-client'
 class DataController < ApplicationController
 
   def retrieve
-    url = "https://api.yelp.com/v3/businesses/search?location=#{params[:location]}&term=#{params[:term]}"
+    url = "https://api.yelp.com/v3/businesses/search?location=#{params[:location]}&term=#{params[:term]}&limit=#{params[:limit]}"
     begin
       data = RestClient.get(url, headers={
         'Authorization': ENV['BEARER_TOKEN']
